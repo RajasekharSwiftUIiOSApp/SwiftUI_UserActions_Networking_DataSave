1b5e9e4239fbdb8883baf991419144eed0b2f444
Raja <Rajasekhar.iosswift@gmail.com>
2024-06-27 08:55:39 +0530

    Initial Commit

---
+++ SwiftUI_UserActions_Networking_DataSave/SwiftUI_UserActions_Networking_DataSaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_UserActions_Networking_DataSaveApp.swift
+//  SwiftUI_UserActions_Networking_DataSave
+//
+//  Created by rajasekhar pidikiti on 27/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_UserActions_Networking_DataSaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
